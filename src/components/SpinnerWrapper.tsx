af0f9e2e20c722eaa99281b1df3b39bf3bd0357d
이준성 <junsung.lee@MB-junsunglee.local>
2022-06-09 09:46:25 +0900

    chore:스피너 스타일 커스텀

--- src/components/SpinnerWrapper.tsx
+++ src/components/SpinnerWrapper.tsx
@@ -1,6 +1,7 @@
 import { Spinner } from "@meshkorea/vroong-design-system-web";
 import { observer } from "mobx-react";
 import React from "react";
+import styled from "styled-components";
 
 import { useCore } from "core";
 
@@ -9,12 +10,25 @@ const SpinnerWrapper = observer(() => {
   const { isSpinnerShow, isSpinnerBlocking, spinnerMessage } = core.dialog;
 
   return (
-    <>
+    <Custom>
       {isSpinnerShow && (
         <Spinner blocking={isSpinnerBlocking} message={spinnerMessage} />
       )}
-    </>
+    </Custom>
   );
 });
 
 export default SpinnerWrapper;
+
+const Custom = styled.div`
+  svg {
+    /* background-color: #fff;
+    border-radius: 50%; */
+    path:nth-child(1) {
+      fill: #fff;
+    }
+    path:nth-child(2) {
+      fill: #435d7a;
+    }
+  }
+`;
