379d2c360e801aaf649e3bdccba12c684fd4388b
이준성 <junsung.lee@MB-junsunglee.local>
2022-07-26 14:10:28 +0900

    chore: 폼 항목 최대개수 추가

--- src/modules/modalForm/ModalFormStore.ts
+++ src/modules/modalForm/ModalFormStore.ts
@@ -64,7 +64,7 @@ export default class ModalFormStore {
 
   @action.bound
   public createListItem() {
-    if (this.form.items.length >= 10) {
+    if (this.form.items.length >= 12) {
       return;
     }
 
